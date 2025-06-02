c243cfa4400f8752b276a78ec14d18372c882e72
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-03 00:03:26 +0530

    sample vm creation

---
+++ 01-resource/main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "test-vm"
+  location              = "UK West"
+  resource_group_name   = "devops_project_ecom"
+  network_interface_ids = ["/subscriptions/4a491ea7-cd6d-4ec6-aa18-28b31973e70c/resourceGroups/devops_project_ecom/providers/Microsoft.Network/networkInterfaces/terraform-testing"]
+  vm_size               = "Standard_D2ls_v5"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    id = "/subscriptions/4a491ea7-cd6d-4ec6-aa18-28b31973e70c/resourceGroups/devops_project_ecom/providers/Microsoft.Compute/images/local-devops-practice"
+  }
+  storage_os_disk {
+    name              = "test-vm-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-vm"
+    admin_username = "harshal"
+    admin_password = "harshal@12345"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
