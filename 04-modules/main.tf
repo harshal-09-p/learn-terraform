7a6d76b270c73ca4594db2ab78432a687af4ca45
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-04 21:08:50 +0530

    variable in module

--- 04-modules/main.tf
+++ 04-modules/main.tf
@@ -1,3 +1,13 @@
 module "sample1" {
   source = "./sample"
+  x = 100
+}
+
+variable "x" {
+  default = 100
+}
+
+module "sample2" {
+  source = "./sample"
+  x = var.x
 }
--- 04-modules/sample/main.tf
+++ 04-modules/sample/main.tf
@@ -2,3 +2,5 @@ resource "null_resource" "test1" {
 }
 resource "null_resource" "test2" {
 }
+
+variable "x" {}
