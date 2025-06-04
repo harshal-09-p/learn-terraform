1fbd2a0771ad5512989f05d5d051fed83253d998
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-05 00:09:50 +0530

    loop1

--- 06-loops-count/main.tf
+++ 06-loops-count/main.tf
@@ -5,3 +5,13 @@
  output "testprint" {
    value = null_resource.test
  }
+
+ output "testprint2" {
+   value = null_resource.test[*].id
+ }
+
+ resource "null_resource" "test1" { }
+
+ output "test1print" {
+   value = null_resource.test1.id
+ }
