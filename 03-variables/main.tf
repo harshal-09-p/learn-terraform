cef487177eafad2407a2ac2e64a8d30c23277c9b
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-04 16:58:49 +0530

    var2

--- 03-variables/main.tf
+++ 03-variables/main.tf
@@ -6,3 +6,50 @@ output "print1" {
     value = var.x
 }
 
+variable "str" {
+    default = "harsh"
+}
+
+variable "num" {
+    default = 10  
+}
+
+variable "mybool" {
+    default = true
+}
+
+output "str" {
+    value = "${ var.str }, hello"
+}
+output "str2" {
+    value = var.str
+}
+
+variable "a" {
+    default = "this is a normal variable"
+}
+
+variable "b" {
+    default = [
+        1,
+        2,
+        "abc",
+        true
+    ]
+}
+
+variable "c" {
+    default = {
+        x = 10,
+        y = 20,
+        z = "abc"
+    }
+}
+
+output "prlist" {
+    value = var.b[1]
+}
+
+output "prmap" {
+    value = var.c["y"]
+}
