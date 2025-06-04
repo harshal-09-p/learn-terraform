5aa2b90c54716eca21116c43e963b043a9eadc61
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-05 00:35:17 +0530

    count 2

--- 06-loops-count/main.tf
+++ 06-loops-count/main.tf
@@ -15,3 +15,57 @@
  output "test1print" {
    value = null_resource.test1.id
  }
+
+ variable "nodes" {
+    default = [
+        "test1",
+        "test2"
+    ]
+ }
+
+ resource "azurerm_network_interface" "privateip" {
+  count                 = length(var.nodes)
+  name                  = "${var.nodes[count.index]}"
+  location              = "UK West"
+  resource_group_name   = "project-setup-1"
+
+  ip_configuration {
+    name                          = "${var.nodes[count.index]}"
+    subnet_id                     = "/subscriptions/4a491ea7-cd6d-4ec6-aa18-28b31973e70c/resourceGroups/devops_project_ecom/providers/Microsoft.Network/virtualNetworks/workstation-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "test" {
+  count                         = length(var.nodes)
+  name                          = "${var.nodes[count.index]}"
+  location                      = "UK West"
+  resource_group_name           = "project-setup-1"
+  network_interface_ids         = [azurerm_network_interface.privateip[count.index].id]
+  vm_size                       = "Standard_B2s"
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+  
+  storage_image_reference {
+    id = "/subscriptions/4a491ea7-cd6d-4ec6-aa18-28b31973e70c/resourceGroups/devops_project_ecom/providers/Microsoft.Compute/images/image"
+  }
+  
+  storage_os_disk {
+    name              = "${var.nodes[count.index]}-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  
+  os_profile {
+    computer_name  = "${var.nodes[count.index]}"
+    admin_username = "harshal"
+    admin_password = "harshal@12345"
+  }
+  
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
